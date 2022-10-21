a1ecf9c2e62406b35a26bbd0cfb894a2fa144714
saurabhcedcoss <saurabhtripathi@cedcoss.com>
2022-10-21 22:50:21 +0530

    add to cart feature

--- src/products.js
+++ src/products.js
@@ -28,4 +28,74 @@ function dynDisp(showcont) {
   document.getElementById("products").innerHTML = maindiv;
 }
 dynDisp(products);
- 
+function cartDisp() {
+    if(cart.length<1){
+      cartTble = "YOUR SHOPPING CART IS EMPTY! START ADDING PRODUCTS";
+    }else{
+    var grandTotal = 0;
+    var cartTble =
+      "<div class='Notification'></div><div class='Header'><h3 class='Heading'>Shopping Cart</h3><h5 class='Action'>Remove all</h5></div>";
+    cart.forEach((element) => {
+      cartTble +=
+        "<div class='Cart-Items'><div class='image-box'><img src='./images/" +
+        products[element.id].image +
+        "' style={{ height='120px' }} /></div><div class='about'><h1 class='title'>" +
+        products[element.id].name +
+        "</h1><br><h3 class='subtitle'>ID:" +
+        products[element.id].id +
+        "</h3></div><div class='counter'><div class='btnm' value='" +
+        element.id +
+        "'>-</div><div class='count'>" +
+        element.quantity +
+        "</div>  <div class='btnp' value='" +
+        element.id +
+        "'>+</div></div><div class='prices'><div class='amount'>$" +
+        products[element.id].price +
+        "</div> <div class='Subtotal'>Sub-Total</div><div class='items'>$" +
+        element.quantity * products[element.id].price +
+        "</div>  <div class='remove'><u>Remove</u></div></div></div>";
+      grandTotal = grandTotal + element.quantity * products[element.id].price;
+    });
+    cartTble +=
+      "<hr><div class='checkout'><div class='total'><div><div class='Subtotal'>Sub-Total</div><div class='items'>" +
+      cart.length +
+      " items</div></div><div class='total-amount'>$" +
+      grandTotal +
+      "</div></div><button class='button'>Checkout</button></div>";
+  }
+    document.getElementById("cart").innerHTML = cartTble;
+  }
+  cartDisp();
+  // add to cart
+  //cartDisp();
+  $(document).ready(function () {
+    $(".add-to-cart").on("click", function () {
+      //if cart is blank
+      var get_pr_id = $(this).attr("value");
+      var prQuan = 1;
+      if (cart.length == 0) {
+        var proToCart = {
+          id: get_pr_id,
+          quantity: prQuan,
+        };
+        cart.push(proToCart);
+      } else if (cart.length > 0) {
+        var flag = 0;
+        cart.forEach((element) => {
+          if (element.id == get_pr_id) {
+            flag = 1;
+            element.quantity = element.quantity + 1;
+          }
+        });
+        if (flag == 0) {
+          var proToCart = {
+            id: get_pr_id,
+            quantity: prQuan,
+          };
+          cart.push(proToCart);
+        }
+      }
+      cartDisp();
+    });
+  });
+  
